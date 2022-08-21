00cde1de04e64d2d18649cee55b7d52061194da0
nductrung1991 <nductrung1991@gmail.com>
2022-08-21 12:15:19 +0700

    init

---
+++ server.js
@@ -0,0 +1,17 @@
+var express = require('express'),
+    app = express(),
+    importData = require("./data.json"),
+    port = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+    res.send("Hello Word");
+});
+
+app.get("/data", (req, res) => {
+    res.send(importData);
+})
+
+//listen for request on port 3000, and as a callback function have the port listened on logged
+app.listen(port, () => {
+    console.log(`Server running at http://${hostName}:${port}/`);
+});
